a1a69fa09473e0e0c9ef7b9db6cbf6d2d1e080f3
aysegulkarakose <akarakose05@gmail.com>
2025-07-08 16:22:27 +0300

    second commit

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-async function getCharacters() {
+async function findCharacter(page = 1) {
+    const input = document.getElementById("searchInput").value.toLowerCase();
+    const url = `https://rickandmortyapi.com/api/character/?name=${input}&page=${page}`;
+
     try {
-        const response = await fetch("https://rickandmortyapi.com/api/character");
+        const response = await fetch(url);
         const data = await response.json();
-        const characters = data.results;
-        const charactersContainer = document.getElementById("characterscontainer");
-
-        characters.forEach(character => {
-            const card = document.createElement("div");
-            card.className = "card";
-            card.innerHTML = `
-        <img src="${character.image}" alt="${character.name}">
-        <h3>${character.name}</h3>
-        <p>${character.status}</p>
-        <p>${character.species}</p>`;
-            card.addEventListener("click", () => {
-                alert(`origin: ${character.origin.name}`);
-            });
-
-            charactersContainer.appendChild(card);
-        });
+        showCharacters(data.results);
+        showPagination(data.info.pages);
     }
     catch (error) {
-        console.error("hata", error);
+        console.error("Bulunamadi", error);
+        document.getElementById("pagination").innerHTML = "";
     }
 }
-getCharacters();
+
+function showCharacters(characters) {
+    const container = document.getElementById("characterscontainer");
+    container.innerHTML = "";
+
+    characters.forEach(character => {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+            <img src="${character.image}" alt="${character.name}">
+            <h3>${character.name}</h3>
+            <p>${character.status}</p>
+            <p>${character.species}</p>
+        `;
+        card.addEventListener("click", () => {
+            alert(`origin: ${character.origin.name}`);
+        })
+        container.appendChild(card);
+    });
+}
+
+function showPagination(totalPages) {
+    const pagination = document.getElementById("pagination");
+    pagination.innerHTML = "";
+
+    for (let i = 1; i <= totalPages; i++) {
+        const button = document.createElement("button");
+        button.textContent = i;
+        button.className = "button";
+        button.addEventListener("click", () => findCharacter(i));
+        pagination.appendChild(button);
+    }
+}
+
+window.onload = () => {
+    findCharacter();
+}
